9e2c19924f24154e5ca411d8b72ed831fd1a15f2
Khem M <khem@DESKTOP-TPCGJAD>
2024-09-30 10:52:19 +0700

    fix(example4): tutorial fetching api

--- Example4/script.js
+++ Example4/script.js
@@ -1,55 +1,25 @@
-async function getRecipeSuggestions() {
-  const url = 'https://tasty.p.rapidapi.com/recipes/list';
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '',//your own API,
-      'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-    }
-  };
-
+async function callApi() {
   try {
-    const response = await fetch(url, options);
-    const data = await response.json();
-    console.log('Data:', data); 
-    console.log('Data properties:', Object.keys(data));
-
-    const recipeList = document.getElementById('recipeList');
-    recipeList.innerHTML = ''; // Clear the list
-
-    console.log(recipeList);
-    console.log('Data:', data); 
-    console.log('Data length:', data.length);
-    console.log('Entering if block');
-
-    if (Array.isArray(data.results)) {
-      console.log('Entering forEach loop');
-      console.log('Recipe List:', recipeList);
-
-      for (let i = 0; i < data.results.length; i++) {
-        const recipe = data.results[i];
-        console.log('Recipe:', recipe);
-        console.log('Recipe Name:', recipe.name);
-
-        const listItem = document.createElement('li');
-        listItem.classList.add('recipeCard');
-
-        const nameElement = document.createElement('p');
-        nameElement.classList.add('recipeName');
-        nameElement.textContent = recipe.name;
-
-        const descriptionElement = document.createElement('p');
-        descriptionElement.classList.add('recipeDescription');
-        descriptionElement.textContent = recipe.description;
-
-        listItem.appendChild(nameElement);
-        listItem.appendChild(descriptionElement);
-
-        recipeList.appendChild(listItem);
-      }
-    } else {
-      console.error('Data is not an array');
-    }
+    const url = 'https://jsonplaceholder.typicode.com/posts/1';
+    const data = await fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();
+      })
+      .then(data => {
+        return data;
+      })
+      .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+      });
+      console.log(data);
+      document.getElementById("my-id").innerHTML = `
+        <br />
+        <div>id: ${data.id}</div>
+        <div>title: ${data.title}</div>
+      `;
   } catch (error) {
     console.error(error);
   }
